e8961e8022fbdd7ad2834da8309bad51ae70388e
Marchello <129538566+Marchello0509@users.noreply.github.com>
2023-11-03 20:21:28 +0200

    0

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const formEl = {
+/* const formEl = {
     inputName: document.querySelector('#inp1'),
     inputText: document.querySelector('#inp2'),
     button: document.querySelector('#btn'),
@@ -16,4 +16,52 @@ formEl.button.addEventListener('click', () => {
         </div>
     </div>
     `
-})
+}) */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const postsContainer = document.querySelector('.postsContainer');
+    const formEl = {
+        name: document.querySelector('#form_name'),
+        text: document.querySelector('#form_text'),
+        date: document.querySelector('#form_date'),
+        btn: document.querySelector('#form_btn')
+    };
+
+    let posts = [
+        {
+            name: "Alex",
+            text: "Some text"
+        }
+    ];
+
+    const renderPosts = () => {
+        postsContainer.innerHTML = '';
+        posts.forEach(postItem => {
+            postsContainer.innerHTML += `
+            <div class="postItem">
+                <p>${postItem.name}</p>
+                <p>${postItem.date}</p>
+                <p>${postItem.text}</p>
+            </div>
+            `;
+        });
+    };
+
+    if (formEl.btn) {
+        formEl.btn.addEventListener('click', () => {
+            posts = [
+                ...posts,
+                {
+                    name: formEl.name.value,
+                    date: formEl.date.value,
+                    text: formEl.text.value
+                }
+            ];
+            formEl.name.value = '';
+            formEl.text.value = '';
+            renderPosts();
+        });
+    }
+
+    renderPosts();
+});
